3f8220318677dc3053c824a4e8e01736fb13f2d4
moyu <3040114965@qq.com>
2025-03-16 22:14:16 +0800

    查询消息的接口

--- msg-service/src/main/java/org/wms/msg/controller/MsgController.java
+++ msg-service/src/main/java/org/wms/msg/controller/MsgController.java
@@ -1,22 +1,23 @@
 package org.wms.msg.controller;
 
-import jakarta.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.wms.common.constant.MQConstant;
+import org.springframework.security.core.parameters.P;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import org.wms.common.entity.msg.Msg;
 import org.wms.common.enums.msg.MsgReadEnums;
-import org.wms.common.enums.msg.MsgTypeEnums;
+import org.wms.common.exception.BizException;
 import org.wms.common.model.Result;
-import org.wms.msg.consumer.MsgConsumer;
-import org.wms.common.entity.msg.WsMsgDataVO;
-import org.wms.common.enums.msg.MsgEnums;
 import org.wms.msg.service.MsgService;
 import org.wms.security.util.SecurityUtil;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import jakarta.annotation.Resource;
 
 @RestController
 @RequestMapping("/msg")
@@ -27,7 +28,6 @@ public class MsgController {
 
     @Resource
     RabbitTemplate rabbitTemplate;
-
 
     /**
      * 统计当前用户是否含有未读的消息
@@ -45,15 +45,102 @@ public class MsgController {
         return Result.success(count > 0, "查询成功");
     }
 
-    @GetMapping("/{id}")
-    public String test(@PathVariable("id") String id) {
-        Msg msg = new Msg();
-        msg.setSenderName("测试");
-        msg.setType(MsgTypeEnums.OTHER);
-        msg.setTitle("测试发送");
-        msg.setContent("你有一条订单需要审批");
-        rabbitTemplate.convertAndSend(MQConstant.EXCHANGE_NAME, MQConstant.ROUTING_KEY,
-                new WsMsgDataVO<>(msg, MsgEnums.NOTICE.getCode(), id));
-        return "发送成功";
+    /**
+     * 分页查询消息
+     *
+     * @param senderId   发送者ID
+     * @param type       消息类型
+     * @param title      消息标题
+     * @param readStatus 阅读状态
+     * @param priority   优先级
+     */
+    @GetMapping("/page")
+    @PreAuthorize("isAuthenticated()")
+    public Result<Page<Msg>> page(
+            String senderId,
+            String type,
+            String title,
+            Integer readStatus,
+            Integer priority,
+            LocalDateTime startTime,
+            LocalDateTime endTime,
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "10") Integer pageSize) {
+
+        String userId = SecurityUtil.getUserID();
+        Page<Msg> pageResult = msgService.lambdaQuery()
+                .eq(Msg::getRecipientId, userId)
+                .eq(StringUtils.hasText(senderId), Msg::getSenderId, senderId)
+                .eq(StringUtils.hasText(type), Msg::getType, type)
+                .like(StringUtils.hasText(title), Msg::getTitle, title)
+                .eq(readStatus != null, Msg::getReadStatus, readStatus)
+                .eq(priority != null, Msg::getPriority, priority)
+                .ge(startTime != null, Msg::getSendTime, startTime)
+                .le(endTime != null, Msg::getSendTime, endTime)
+                .orderByAsc(Msg::getReadStatus)
+                .orderByDesc(Msg::getSendTime)
+                .page(new Page<>(page, pageSize));
+
+        return Result.success(pageResult, "查询成功");
+    }
+
+    /**
+     * 获取未读消息
+     *
+     * @return 未读消息列表
+     */
+    @GetMapping("/getUnReadMsg")
+    @PreAuthorize("isAuthenticated()")
+    public Result<List<Msg>> getUnReadMsg() {
+        String userId = SecurityUtil.getUserID();
+
+        List<Msg> unreadMsgs = msgService.lambdaQuery()
+                .eq(Msg::getRecipientId, userId)
+                .eq(Msg::getReadStatus, MsgReadEnums.UNREAD.getCode())
+                .orderByDesc(Msg::getSendTime)
+                .last("LIMIT 10")
+                .list();
+
+        return Result.success(unreadMsgs, "查询成功");
+    }
+
+    /**
+     * 获取已读消息
+     *
+     * @return 已读消息列表
+     */
+    @GetMapping("/getReadMsg")
+    @PreAuthorize("isAuthenticated()")
+    public Result<List<Msg>> getReadMsg() {
+        String userId = SecurityUtil.getUserID();
+
+        List<Msg> readMsgs = msgService.lambdaQuery()
+                .eq(Msg::getRecipientId, userId)
+                .eq(Msg::getReadStatus, MsgReadEnums.READ.getCode())
+                .orderByDesc(Msg::getSendTime)
+                .last("LIMIT 10")
+                .list();
+
+        return Result.success(readMsgs, "查询成功");
+    }
+
+    /**
+     * 读消息
+     *
+     * @param id id
+     * @return 返回结果
+     */
+    @PutMapping("/read/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public Result<String> read(@PathVariable("id") String id) {
+        boolean update = msgService.lambdaUpdate()
+                .eq(Msg::getId, id)
+                .set(Msg::getReadStatus, MsgReadEnums.READ.getCode())
+                .set(Msg::getReadTime, LocalDateTime.now())
+                .update();
+        if (!update) {
+            throw new BizException("修改状态失败");
+        }
+        return Result.success(null, "修改状态成功");
     }
 }
