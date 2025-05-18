2fbacace311ebf520c753c7f7f5012b5a260d38a
lsh <3040114965@qq.com>
2025-05-18 10:55:59 +0800

    新增订单数量字段

--- order-service/src/main/java/org/wms/order/model/vo/OrderStatisticsVo.java
+++ order-service/src/main/java/org/wms/order/model/vo/OrderStatisticsVo.java
@@ -6,8 +6,9 @@ import java.math.BigDecimal;
 
 @Data
 public class OrderStatisticsVo {
-    Integer status;
-    String statusVo;
-    BigDecimal totalAmount;
-    Integer totalQuantity;
+    Integer status; // 订单状态
+    String statusVo; // 状态描述
+    Integer count; // 订单数
+    BigDecimal totalAmount; // 订单总金额
+    Integer totalQuantity; // 总商品数量
 }
